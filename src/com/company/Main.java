82139446a59a74685d24617d3d8bd9abeb7123b9
Dubakh <dubakh@gmail.com>
2017-12-06 20:55:44 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.*;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    //File file = new File("/Users/boris/Desktop/test.txt");
+        // InputStream inputStream = new FileInputStream(file);
+        InputStream inputStream = new URL("https://www.google.co.il/").openStream();
+        OutputStream outputStream = System.out;
+        try {
+            copy(inputStream, outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copy(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte [] buff = new byte[64 * 1024];
+
+        while(inputStream.available() > 0) {
+            int count = inputStream.read(buff);
+
+            outputStream.write(buff, 0, count);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+}
